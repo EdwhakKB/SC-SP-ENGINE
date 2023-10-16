87f7d5013f462f0ec74d259247880e9df68199f3
Edwhak_KB <128205303+EdwhakKB@users.noreply.github.com>
2023-10-15 19:17:08 -0500

    Update PlayState.hx

--- funkinscsource/states/PlayState.hx
+++ funkinscsource/states/PlayState.hx
@@ -5508,7 +5508,7 @@ class PlayState extends MusicBeatState
 		// GUITAR HERO SUSTAIN CHECK LOL!!!!
 		if (note != null && guitarHeroSustains && note.parent == null) {
 			if(note.tail.length > 0) {
-				note.alpha = 0.35;
+				note.alpha *= 0.35;
 				for(childNote in note.tail) {
 					childNote.alpha = note.alpha;
 					childNote.missed = true;
