ee0431fdb22b981fd602b7e4ec5bce48b31d9277
glowsoony <84847356+glowsoony@users.noreply.github.com>
2024-08-22 21:32:36 -0400

    Update order

--- setup/unix.sh
+++ setup/unix.sh
@@ -4,21 +4,21 @@
 # REMINDER THAT YOU NEED HAXE INSTALLED PRIOR TO USING THIS
 # https://haxe.org/download/version/4.3.4/
 haxelib install lime
-haxelib install openfl
-haxelib install flixel
+haxelib git openfl https://github.com/FunkinCrew/openfl feature/shader-improvements
+haxelib git flixel https://github.com/glowsoony/flixel/tree/Main-SCE
+haxelib git flixel-addons https://github.com/glowsoony/flixel-addons/tree/dev
 haxelib install tjson
 haxelib install flixel-tools
 haxelib install hscript-iris 1.0.2
 haxelib install hscript
+haxelib git hscript-improved https://github.com/glowsoony/hscript-improved/tree/custom-classes
 haxelib install hxgamejolt-api
 haxelib install markdown
-haxelib git flixel https://github.com/glowsoony/flixel/tree/Main-SCE
-haxelib git flixel-addons https://github.com/glowsoony/flixel-addons/tree/dev
+haxelib install format
+haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
+haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate/tree/768740a56b26aa0c072720e0d1236b94afe68e3e
 haxelib git FlxPartialSound https://github.com/FunkinCrew/FlxPartialSound/tree/f986332ba5ab02abd386ce662578baf04904604a
 haxelib git fnf-modcharting-tools https://github.com/glowsoony/FNF-Modcharting-Tools main-old
-haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
-haxelib git hscript-improved https://github.com/glowsoony/hscript-improved/tree/custom-classes
-haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate/tree/768740a56b26aa0c072720e0d1236b94afe68e3e
 haxelib git hxdiscord_rpc https://github.com/FNF-CNE-Devs/hxdiscord_rpc/tree/main
 haxelib git grig.audio https://gitlab.com/haxe-grig/grig.audio.git cbf91e2180fd2e374924fe74844086aab7891666
 haxelib git funkin.vis https://github.com/FunkinCrew/funkVis d5361037efa3a02c4ab20b5bd14ca11e7d00f519
