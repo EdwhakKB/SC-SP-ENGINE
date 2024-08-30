614a8a56093db932dc97b8d3e505a9097fe3ec93
glowsoony <84847356+glowsoony@users.noreply.github.com>
2024-08-30 10:49:18 -0400

    Quick fix

--- setup/Main.hx
+++ setup/Main.hx
@@ -8,12 +8,12 @@ import sys.io.File;
 
 typedef Library =
 {
-  name:String,
-  type:String,
-  version:String,
-  dir:String,
-  ref:String,
-  url:String
+  var name:String;
+  var type:String;
+  var ?version:String;
+  var ?dir:String;
+  var ?ref:String;
+  var ?url:String;
 }
 
 class Main
@@ -34,16 +34,13 @@ class Main
       {
         case "install", "haxelib": // for libraries only available in the haxe package manager
           var version:String = data.version == null ? "" : data.version;
-          Sys.command('haxelib --quiet install ${data.name} ${version}');
+          Sys.command('haxelib install ${data.name} ${version} --always');
         case "git": // for libraries that contain git repositories
           var ref:String = data.ref == null ? "" : data.ref;
-          Sys.command('haxelib --quiet git ${data.name} ${data.url} ${data.ref}');
+          Sys.command('haxelib git ${data.name} ${data.url} ${ref} --always');
         default: // and finally, throw an error if the library has no type
           Sys.println('[PSYCH ENGINE SETUP]: Unable to resolve library of type "${data.type}" for library "${data.name}"');
       }
     }
-
-    // after the loop, we can leave
-    Sys.exit(0);
   }
 }
